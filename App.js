635b91e5e7bc57c1b46b39972a45509a236e2fd5
YERIM <dnaer456@gmail.com>
2021-03-06 18:27:23 +0900

    first

--- App.js
+++ App.js
@@ -1,21 +1,155 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
-export default function App() {
+/* 현재 디바이스의 width 값 가져오기 - Dimensions */
+
+const CURRENT_WIDTH = Dimensions.get(`window`).width;
+
+const App = () => {
+  const [tab, setTab] = useState(0);
+
+  const _startButtonClickHandler = (value) => {
+    setTab(value);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.ingameArea}>
+        {tab === 0 && (
+          <TouchableOpacity
+            style={styles.startBtn}
+            onPressOut={() => _startButtonClickHandler(1)}
+          >
+            <Text style={styles.startBtnText}>Start Game</Text>
+          </TouchableOpacity>
+        )}
+        {tab === 1 && (
+          <View>
+            <View style={styles.ingameTop}>
+              <Text>TOP</Text>
+            </View>
+
+            <View style={styles.ingameMiddle}>
+              <LinearGradient
+                colors={["#939696", "#addbdb"]}
+                style={styles.vsView}
+              >
+                <Text style={styles.vsText}>VS</Text>
+              </LinearGradient>
+            </View>
+
+            <View style={styles.ingameBottom}>
+              <Text>BOTTOM</Text>
+            </View>
+          </View>
+        )}
+      </View>
+      <View style={styles.resultArea}>
+        <View style={styles.resultAreaTop}></View>
+        <View style={styles.resultAreaBottom}>
+          {tab === 1 && (
+            <TouchableOpacity
+              style={styles.startBtn}
+              onPressOut={() => _startButtonClickHandler(0)}
+            >
+              <Text style={styles.startBtnText}>RESTART!</Text>
+            </TouchableOpacity>
+          )}
+        </View>
+      </View>
     </View>
   );
-}
+};
+
+/* 삼항연산자 -> (조건식) ? true일 때 : false일 때 */
+/* javascript optional -> (조건식) ? true라면 && ~~~ */
+/* 속도는 삼항연산자가 더 빠름 */
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  ingameArea: {
+    flex: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  resultArea: {
+    flex: 2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  startBtn: {
+    width: CURRENT_WIDTH / 2,
+
+    height: 45,
+    backgroundColor: "#5f6061",
+    borderRadius: 6,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  startBtnText: {
+    color: "#fff",
+
+    fontWeight: "700",
+    fontSize: 20,
+  },
+
+  resultAreaTop: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  resultAreaBottom: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  ingameTop: {
+    flex: 4,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  ingameMiddle: {
+    flex: 2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  ingameBottom: {
+    flex: 4,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  vsView: {
+    width: CURRENT_WIDTH,
+    height: 50,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  vsText: {
+    color: "#fff",
+
+    fontSize: 20,
+    fontWeight: "600",
   },
 });
+
+export default App;
